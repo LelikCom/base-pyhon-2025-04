a633b207e06ab79388af04c6a59a0ca75671a482
Ичетовкин Алексей <IchetovkinAlexei@yandex.ru>
2025-04-02 23:32:11 +0500

    fix pylint

--- testing/test_app.py
+++ testing/test_app.py
@@ -1,3 +1,11 @@
-def test_index(client):
-    response = client.get("/")
-    assert 200  == response.status_code
+import pytest
+from fastapi.testclient import TestClient
+from app import app
+
+@pytest.fixture
+def test_client():
+    return TestClient(app)
+
+def test_index(test_client):
+    response = test_client.get("/")
+    assert response.status_code == 200
